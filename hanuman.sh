69f9ff68164833d96ff1118382d5d9b529865f6e
Tejareddy-19 <p.tejashwarreddy9@gmail.com>
2024-02-22 11:10:44 +0000

    the two are added

---
+++ hanuman.sh
@@ -0,0 +1,2 @@
+the hanuman is orginated from the character of lord shive
+the ultimate super comsic powers within him
---
+++ tejareddy.java
@@ -0,0 +1,3 @@
+the teja reddy is the good life leader 
+he always very good with people with spreading love
+
